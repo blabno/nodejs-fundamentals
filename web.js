f0ac06ff2dbce5a623450e81ebd4d076280b4f87
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2018-07-07 07:33:41 +0200

    Web server

--- web.js
+++ web.js
@@ -2,4 +2,20 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
+// Create a server
+const port = 8080;
+http.createServer(function (request, response) {
+  const parsedUrl = url.parse(request.url);
+  const pathname = parsedUrl.pathname;
 
+  console.log('Request for ' + pathname + ' received.');
+
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write('<h1>Hello World!</h1>');
+  response.write(`<pre>${JSON.stringify(parsedUrl, null, 2)}</pre>`);
+  response.end();
+
+}).listen(port);
+
+// Console will print the message
+console.log(`Server running at http://127.0.0.1:${port}/`);
